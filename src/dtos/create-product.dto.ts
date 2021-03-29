ff22aba1ed68f2e71a25c36a652e7023a1958902
edsonsousa33 <edson.sousa@compasso.com.br>
2021-03-29 13:25:31 -0300

    + validation createProduct

--- src/dtos/create-product.dto.ts
+++ src/dtos/create-product.dto.ts
@@ -1,6 +1,18 @@
+import { isNotEmpty, IsNotEmpty, IsNumber, IsString } from 'class-validator'
 export class CreateProductDTO {
+    @IsNotEmpty()
+    @IsString()
     name: string;
+
+    @IsNotEmpty()
+    @IsNumber()
     price: number;
+
+    @IsNotEmpty()
+    @IsString()
     description: string;
+
+    @IsNotEmpty()
+    @IsNumber()
     inventory: number;
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,14 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreateProductDTO } from 'src/dtos/create-product.dto';
 import { IProduct } from 'src/interfaces/product.interface';
 import { ProductsService } from './products.service';
 
 @Controller('api/v1/products')
 export class ProductsController {
-    constructor(private productService: ProductsService){}
+  constructor(private productService: ProductsService) {}
 
-    @Post()
-    async createProduct(@Body() createProductDTO: CreateProductDTO): Promise<IProduct> {
-        return this.productService.save(createProductDTO)
-    }
+  @Post()
+  @UsePipes(ValidationPipe)
+  async createProduct(
+    @Body() createProductDTO: CreateProductDTO,
+  ): Promise<IProduct> {
+    return this.productService.save(createProductDTO);
+  }
 }
